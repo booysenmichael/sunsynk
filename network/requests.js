e4069eddd6b180130425e97a5e07641e8f5caf63
marius booysen <marius@booysen>
2025-02-22 22:09:21 +0200

    Init requests

--- network/requests.js
+++ network/requests.js
@@ -1,2 +1,16 @@
-//const axios = require('axios');
+const axios = require('axios');
+const url = "Thiswillnbetheurlforsunsynkendpoint";
 
+//Let's setup a axios client
+const request = axios.create({
+    baseURL:url
+})
+
+const getInfo = async ()=>{
+    const response = await request.get();
+    return response
+}
+
+module.exports = {
+    getInfo
+}
